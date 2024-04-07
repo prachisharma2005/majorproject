39987945866bead73e70dbcc371a793d2285626c
Prachi <rs804428@gmail.com>
2024-04-07 12:19:12 +0530

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const listing = require("./models/listings.js");
+const listing = require("./models/listing.js");
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
 const mongourl ="mongodb://127.0.0.1:27017/wanderlust";
 main()
 .then(()=>{
@@ -15,21 +20,24 @@ async function main() {
     await mongoose.connect(mongourl);
 }
 
-app.get("/testlisting",async (req,res)=>{
-    let smapleListing = new listing({
-        title:"my new villa",
-        description:"by the beach",
-        price:1200,
-        location:"goa",
-        country:"india",
-    });
+// app.get("/testlisting",async (req,res)=>{
+//     let smapleListing = new listing({
+//         title:"my new villa",
+//         description:"by the beach",
+//         price:1200,
+//         location:"goa",
+//         country:"india",
+//     });
 
-    await smapleListing.save();
-    console.log("sample was saved");
-    res.send("successful");
-});
+//     await smapleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful");
+// });
 
-
+app.get("/listings",async (req,res)=>{
+    const alllistings = await listing.find({});
+    res.render("/listings/index.ejs", {alllistings});
+  });
 
  
 app.get("/",(req,res)=>{
---
+++ init/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const listing = require("../models/listing.js");
+const mongourl ="mongodb://127.0.0.1:27017/wanderlust";
+main()
+.then(()=>{
+    console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(mongourl);
+}
+
+const initDB = async()=>{
+    await listing.deleteMany({});
+    await listing.insertMany(initData.data);
+    console.log("data was initialised");
+
+};
+initDB();
